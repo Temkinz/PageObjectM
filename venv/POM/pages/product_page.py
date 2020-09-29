efd417cd08578aa130366388532568d7e78f8037
Temkinz <50596199+Temkinz@users.noreply.github.com>
2020-09-29 22:12:03 +0300

    added params

--- venv/POM/pages/product_page.py
+++ venv/POM/pages/product_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoAlertPresentException
 import math
 
 
@@ -7,13 +8,11 @@ class ProductPage(BasePage):
     PRODUCT_ADD_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     BOOK_TITLE = (By.CSS_SELECTOR, "div h1")
     PRICE = (By.CSS_SELECTOR, "div.product_main > p.price_color")
-    BOOK_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+    BOOK_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) div > strong")
     PRICE_MESSAGE = (By.CSS_SELECTOR, "#messages p:nth-child(1) > strong")
 
     def add_product_to_cart(self):
         add_button = self.browser.find_element(*self.PRODUCT_ADD_BUTTON).click()
-
-    from selenium.common.exceptions import NoAlertPresentException  # в начале файла
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
@@ -29,24 +28,18 @@ class ProductPage(BasePage):
         except NoAlertPresentException:
             print("No second alert presented")
 
-    #def get_book_title(self):
-        #book_title = self.browser.find_element(*self.BOOK_TITLE).text
-        #return book_title
+    def get_title(self):
+        book_title = self.browser.find_element(*self.BOOK_TITLE).text
+        return book_title
 
-    #def get_price(self):
-        #price = self.browser.find_element(*self.PRICE).text
-        #return price
+    def get_price(self):
+        price = self.browser.find_element(*self.PRICE).text
+        return price
 
-    #def get_book_message(self):
-        #book_message = self.browser.find_element(*self.BOOK_MESSAGE).text
-        #return book_message
+    def should_be_product_in_cart(self, book_title):
+        book_message = self.browser.find_element(*self.BOOK_MESSAGE).text
+        assert book_title == book_message
 
-    #def get_price_message(self):
-        #price_message = self.browser.find_element(*self.PRICE_MESSAGE).text
-        #return price_message
-
-    def should_be_product_in_cart(self):
-        assert self.browser.find_element(*self.BOOK_TITLE).text in self.browser.find_element(*self.BOOK_MESSAGE).text
-
-    def should_be_correct_price(self):
-        assert self.browser.find_element(*self.PRICE).text in self.browser.find_element(*self.PRICE_MESSAGE).text
+    def should_be_correct_price(self, price):
+        price_message = self.browser.find_element(*self.PRICE_MESSAGE).text
+        assert price == price_message
--- venv/POM/test_product_page.py
+++ venv/POM/test_product_page.py
@@ -1,11 +1,23 @@
 from .pages.product_page import ProductPage
+import pytest
 
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_cart()
-    page.solve_quiz_and_get_code()
-    page.should_be_product_in_cart()
-    page.should_be_correct_price()
+def test_guest_can_add_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    book_title = product_page.get_title()
+    price = product_page.get_price()
+    product_page.add_product_to_cart()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_product_in_cart(book_title)
+    product_page.should_be_correct_price(price)
